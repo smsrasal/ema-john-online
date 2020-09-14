0de9c047cfeb94fd69bc2ae47d13132d0fdfdec4
unknown <smraselcoxr@gmail.com>
2020-09-14 11:31:21 +0600

    react rout connection

---
+++ src/components/ProductDetail/ProductDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetail = () => {
+    return (
+        <div>
+            <h1>Product Detail Coming soooooon</h1>
+        </div>
+    );
+};
+
+export default ProductDetail;
